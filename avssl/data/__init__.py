4e3c7832f29fa702bf7405c05b8d7cc50c6aadfa
atosystem <yjshih23@gmail.com>
2022-04-03 18:00:40 +0800

    finished general dataset for flickr

--- avssl/data/__init__.py
+++ avssl/data/__init__.py
@@ -1,5 +1,5 @@
 from .audio_transforms import random_crop_max_length
-from .collate_function import collate_image_captions
-from .flickr_dataset import FlickrImageCaptionDataset
+from .collate_function import collate_general, collate_image_captions
+from .flickr_dataset import FlickrDataset, FlickrImageCaptionDataset
 from .image_transforms import get_simple_image_transform
 from .places_dataset import PlacesImageCaptionDataset
